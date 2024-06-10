720becd54c0d63d9a026f08cbd6e92c84062199c
smrutiofficial <smrutiprakashrout3@gmail.com>
2024-06-10 17:18:22 +0530

    tyu

--- smblog/pages/index.tsx
+++ smblog/pages/index.tsx
@@ -20,8 +20,18 @@ const index = () => {
                     <Post />
                 </div>
                 {/* -------------------------- */}
-                <div className="w-[25%] border-2 border-green-500 h-full e2">
-
+                <div className="w-[25%] h-full e2">
+                    <div className="cprofilepic relative h-[32rem] flex justify-center items-center">
+                        <div className="pic_poi absolute rounded-lg transform -translate-x-10 -translate-y-10 bg-[#B2B3DE] h-[70%] w-[70%]"></div>
+                        <div className="bgdiv relative flex justify-center items-center rounded-lg bg-[#5D5B9A] h-[70%] w-[70%]">
+                            <div className="imagety bg-black w-[85%] h-[85%] rounded-lg overflow-hidden relative">
+                                <img className="w-full h-full object-cover" src="/self.png" alt="" />
+                            </div>
+                            <div className="prof bg-[#5D5B9A] w-[80%] py-5 text-center font-bold text-white rounded-lg absolute bottom-[3rem] right-8 transform -rotate-90 translate-x-[50%] translate-y-[4%]">
+                                <p>DEVELOPER</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
